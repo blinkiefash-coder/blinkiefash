b0f4019b3c710351b29b3a8896e4ce43e293bca4
Satyam Mohanty <blinkiefash@gmail.com>
2026-05-06 16:18:22 +0530

    fix rename and all

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,14 @@ import categoryRoutes from "./routes/categories.js";
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+    "https://blinkiefash.in"
+  ],
+  methods: ["GET", "POST", "PUT", "DELETE"],
+  credentials: true
+}));
 app.use(express.json());
 
 app.use("/api/products", productRoutes);
