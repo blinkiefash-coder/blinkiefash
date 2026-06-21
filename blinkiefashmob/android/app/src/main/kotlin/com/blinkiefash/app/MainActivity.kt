64416cb546b0a9d429d25a2fe388c65048a21543
Satyam Mohanty <blinkiefash@gmail.com>
2026-06-21 11:47:40 +0530

    Remove duplicate and old blinkiefashmob folders - keeping only blinkiefash/blinkiefashmob

--- blinkiefashmob/android/app/src/main/kotlin/com/blinkiefash/app/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.blinkiefash.app
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- frontend/blinkiefashmob/android/app/src/main/kotlin/com/blinkie_fash/Customer1/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.blinkie_fash.Customer1
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
