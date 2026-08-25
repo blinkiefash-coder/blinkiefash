4771b1c1edebc2316ef1708ef2dd0ef40fc0111d
Satyam Mohanty <blinkiefash@gmail.com>
2026-08-25 10:07:00 +0530

    Show all available Libas products on mobile

--- blinkiefashmob/lib/pages/home_screen.dart
+++ blinkiefashmob/lib/pages/home_screen.dart
@@ -2297,7 +2297,7 @@ class _HomeScreenState extends State<HomeScreen>
                     () => Navigator.of(context).push(
                       MaterialPageRoute(
                         builder: (_) => const AllProductsScreen(
-                          initialSearch: 'Libas Kurti Kurta Set',
+                          initialSearch: 'Libas',
                         ),
                       ),
                     ),
